5657a6588132a9c2e61a0722e2e2a5b1e3e05588
Vinicius Marchi <viniciuspmarchi@gmail.com>
2022-11-16 19:07:23 -0300

    improving the comments

--- hillclimbing/Main.java
+++ hillclimbing/Main.java
@@ -5,30 +5,22 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-  public static boolean validInput(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException {
+    // check if args are valid
     if (args.length < 2) {
       if (args[0].isEmpty()) {
         System.out.println("Enter the number of vertices in the graph.");
-        return false; 
+        return; 
       }	else if (args[1].isEmpty()) {
-        System.out.println("Enter input file path.");
-        return false;
+        System.out.println("Enter a valid input file path.");
+        return;
       }
-    }
-    return true;
-  }
-
-  public static void main(String[] args) throws FileNotFoundException {
-    boolean argsIsValid = Main.validInput(args);
-    // if args is not a valid input
-    if(!argsIsValid) {
-      return;
     }
 
     int vertexQty; // number of vertexs 
     int[][] adjMatrix; // ajacency matrix that represents the cities graph
     int [] route = null; // rout found by the hillclimb algorithm
-    int iterationsNumber;
+    int iterationsNumber; // number of iterations of Hill Climbing algorithm
     try {
       iterationsNumber = Integer.parseInt(args[1]);
     } catch (NumberFormatException e) {
@@ -41,14 +33,13 @@ public class Main {
     
     // Check if is a valid file
     if(file.exists() && file.isFile() && file.canRead()) {
-      System.out.println("Input File: " + args[0]);
+      System.out.println("\n\nInput File: " + args[0]);
       Scanner scanner = new Scanner(file);
-      scanner.useDelimiter("[\\s,]+"); // delimitador das linhas generico
+      scanner.useDelimiter("[\\s,]+"); // generic line delimiter
 
       // The first line of the file defines the number of vertices of the graph
       vertexQty = scanner.nextInt();
       adjMatrix = new int[vertexQty][vertexQty];
-
 
       // Adding the values ​​read from file into the matrix structure
       for(int i=0; i < vertexQty; i++) {
@@ -61,7 +52,6 @@ public class Main {
       // Hill Climbing
       Hill hill = new Hill();
       route = hill.hillClimbing(adjMatrix, iterationsNumber, vertexQty);
-      System.out.println("\n\n");
       System.out.println("Number of Vertexs: " + vertexQty);
       System.out.println("Number of iterations: " + iterationsNumber);
       System.out.println("Solution Found - route: " + Arrays.toString(route));
